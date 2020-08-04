11abeff8adf502f5d4f24e9ea6dcfd027db6fae4
aikaiqiang <@akq930210>
2020-08-04 16:04:07 +0800

    2020-08-04: go sync.Mutex 和 sync.RWMutex 使用，保证线程安全

---
+++ goroutine/rwmutex/demo.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+)
+
+type Person struct {
+	Money   int
+	rwMutex sync.RWMutex
+}
+
+// GetMoney 获取用户金钱
+func (p *Person) GetMoney() int {
+	p.rwMutex.RLock()
+	defer p.rwMutex.RUnlock()
+	money := p.Money
+	return money
+}
+
+// AddMoney 设置用户金钱
+func (p *Person) AddMoney(diff int) {
+	p.rwMutex.Lock()
+	defer p.rwMutex.Unlock()
+	p.Money += diff
+}
+
+func main() {
+	p := Person{Money: 100}
+	go func() {
+		p.AddMoney(1000)
+	}()
+	fmt.Printf("Money: %d\n", p.GetMoney())
+}
